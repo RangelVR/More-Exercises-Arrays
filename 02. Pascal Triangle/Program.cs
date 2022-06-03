fcf916bcb4f3966ab8a7677b3155ef6a964266c0
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-03 23:13:02 +0300

    Update Program.cs

--- 02. Pascal Triangle/Program.cs
+++ 02. Pascal Triangle/Program.cs
@@ -24,12 +24,37 @@ namespace _02._Pascal_Triangle
             foreach (var row in triangle)
             {
                 Console.WriteLine(string.Join(" ", row));
-            }
-           
+            }           
             //for (int i = 0; i < triangle.Length; i++)
             //{
             //    Console.WriteLine(string.Join(" ", triangle[i]));
             //}
+            
+            --------------------second--variant-----------------------------                
+            //int rows = int.Parse(Console.ReadLine());
+            //long[][] triangle = new long[rows][];
+
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    long[] currRow = new long[i + 1];
+            //    currRow[0] = 1;
+            //    currRow[i] = 1;
+
+            //    if (i > 1)
+            //    {
+            //        for (int j = 1; j < i; j++)
+            //        {
+            //            currRow[j] = triangle[i - 1][j] + triangle[i - 1][j - 1];
+            //        }
+            //    }
+            //    triangle[i] = currRow;
+            //}
+            //foreach (var row in triangle)
+            //{
+            //    Console.WriteLine(string.Join(" ", row));
+            //}
+            
+            
         }
     }
 }
