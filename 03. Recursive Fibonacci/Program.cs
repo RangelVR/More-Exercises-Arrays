1f961ae055aa0ab3a796df0f16e3901d24b3d217
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-06 23:03:42 +0300

    Add files via upload

---
+++ 03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03._Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacciNum = int.Parse(Console.ReadLine());
+            long[] fibonacciSequence = new long[50];
+            fibonacciSequence[0] = 1;
+            fibonacciSequence[1] = 1;
+
+            //if (fibonacciNum > 2)
+            //{
+
+            //}
+            for (int i = 2; i < fibonacciNum; i++)
+            {
+                fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+            }
+            Console.WriteLine(fibonacciSequence[fibonacciNum - 1]);
+        }
+    }
+}
