f013a5a636dd8fe6fac70de7f755436113942d60
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-08 21:56:50 +0300

    Update Program.cs

--- 04. Fold and Sum/Program.cs
+++ 04. Fold and Sum/Program.cs
@@ -83,3 +83,35 @@ namespace _04._Fold_and_Sum
 //        Console.WriteLine(string.Join(" ", resultArray));
 //    }
 //}
+
+
+//using System;
+//using System.Linq;
+
+//namespace _04.FoldAndSum
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = Console.ReadLine()
+//                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+//                .Select(int.Parse)
+//                .ToArray();
+
+//            int foldPoint = arr.Length / 2 / 2;
+
+//            int[] resultArr = new int[foldPoint * 2];
+
+//            for (int i = 0; i < foldPoint; i++)
+//            {
+//                resultArr[i] = arr[i + foldPoint] + arr[foldPoint - 1 - i];
+
+//                resultArr[i + foldPoint] = arr[i + 2 * foldPoint] + arr[arr.Length - 1 - i];
+
+//            }
+
+//            Console.WriteLine(String.Join(' ', resultArr));
+//        }
+//    }
+//}
